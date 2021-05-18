a6ec1f0c33b9576b89d8bc09e67d6669cb94ee9f
Nastya Bukina <nastya.bukina@gmail.com>
2021-05-18 23:38:36 +0300

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const [value, setColor] = useState('#db3e00');
+	const colors = [
+		{ name: 'red', color: '#db3e00' },
+		{ name: 'yellow', color: '#fccb00' },
+		{ name: 'green', color: '#008b02' },
+		{ name: 'blue', color: '#1273de' },
+	] 
+	const handleChangeColor = newValue => {
+		setColor(newValue);
+	};
+	return (
+		<div className="wrapper">
+			<ColorPicker value={value} colors={colors} onChange={handleChangeColor}  />
+		</div>
+	);
 }
+
+const ColorPicker = ({value, colors, onChange}) => {
+	const [showHideBlock, setShowHideBlock] = useState('hide');
+	return(
+		<div className="color-picker">
+			<span className="current-color-name">{value}</span>
+			<div className="controls">
+				<div>
+					<span className="default-color" style={{backgroundColor: `${value}`}} onClick={() => setShowHideBlock(showHideBlock == 'show-sliders' ? 'hide' : 'show-sliders')} ></span>
+				</div>
+				<div>
+					<span className="arrow" onClick={() => setShowHideBlock(showHideBlock == 'show-colors' ? 'hide' : 'show-colors')} ></span>
+				</div>
+			</div>
+			{showHideBlock == 'show-colors' ? <DefaultColors value={value} colorsArr={colors} handleChangeColor={onChange} /> : null}
+			{showHideBlock == 'show-sliders' ? <Sliders value={value} handleChangeColor={onChange} /> : null}
+		</div>
+	)
+}
+
+const DefaultColors = ({value, colorsArr, handleChangeColor}) => {
+	const colors = colorsArr;
+	const colorItems = colors.map((item) => 
+		<div className={item.color == value ? 'active' : ''} key={item.color} onClick={() => handleChangeColor(item.color)} >
+			<span className="name">{item.name}</span>
+			<span className="color" style={{backgroundColor: `${item.color}`}}></span>
+		</div>
+	);
+	return (
+		<div className="container-default-colors">
+			{colorItems}
+		</div>
+  )
+}
+
+const hexToRGB = (value) => {
+	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(value);
+	return result ? {
+		red: parseInt(result[1], 16),
+		green: parseInt(result[2], 16),
+		blue: parseInt(result[3], 16)
+	} : null;
+}
+
+const rgbToHex = (r, g, b) => {
+	return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+const Sliders = ({value, handleChangeColor}) => {
+	let rgb = hexToRGB(value);
+	return (
+			<div className="sliders">
+				<div className="red">
+					<label>
+						R
+					</label>
+					<input id="red" type="range" 
+						min="0" 
+						max="255" 
+						steps="1" 
+						value={rgb.red} 
+						onChange={e => handleChangeColor(rgbToHex(e.target.value, rgb.green, rgb.blue))}
+					/>
+				</div>
+				<div className="green">
+					<label>
+						G
+					</label>
+					<input id="green" type="range" 
+						min="0" 
+						max="255" 
+						steps="1" 
+						value={rgb.green} 
+						onChange={e => handleChangeColor(rgbToHex(rgb.red, e.target.value, rgb.blue))} 
+					/>
+				</div>
+				
+				<div className="blue">
+					<label>
+						B
+					</label>
+					<input id="blue" type="range"  
+						min="0" 
+						max="255" 
+						steps="1" 
+						value={rgb.blue} 
+						onChange={e => handleChangeColor(rgbToHex(rgb.red, rgb.green, Number(e.target.value)))} 
+					/>
+				</div>
+		</div>
+	)
+}
+
+
 
 export default App;
